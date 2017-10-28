16504e565480f2e71f5c295e304e7cdbdf5a2ce0
José Mussa <jose.mussa@gmail.com>
2017-10-28 20:15:35 +0200

    Adding logic to prevent regular functions to break on extra arguments

--- src/connectHandlers.js
+++ src/connectHandlers.js
@@ -1,11 +1,22 @@
 import { isFunction, isObject, mapValues } from 'lodash';
 
+const handleExtraArguments = (value, context) => {
+    if (isFunction(value)) {
+        return (params) => {
+            const result = value(params);
+            if (isFunction(result)) {
+                return result(context);
+            }
+            return result;
+        };
+    }
+
+    return value;
+};
+
 const mapExtraArguments = (extraArguments, context) =>
     (isObject(extraArguments)
-        ? mapValues(
-            extraArguments,
-            value => (isFunction(value) ? params => value(params)(context) : value),
-        )
+        ? mapValues(extraArguments, value => handleExtraArguments(value, context))
         : {});
 
 export default function createConnect(handler, props) {
